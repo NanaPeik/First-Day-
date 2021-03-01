c5bccf130cfdc922b67c2a9c43e0fdf07e7fe531
Nana Peikrishvili <nana.feiqrishvili2014@gmail.com>
2021-03-01 16:53:37 +0400

    change findViewById to view binding

--- app/src/main/java/ge/tsu/android/checknumber/MainActivity.java
+++ app/src/main/java/ge/tsu/android/checknumber/MainActivity.java
@@ -2,53 +2,48 @@ package ge.tsu.android.checknumber;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-import java.io.Console;
 import java.util.Random;
+
+import ge.tsu.android.checknumber.databinding.FragmentBlankBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView high_or_Low;
-    EditText editText;
-    Button checkButton;
     public int generateNumber(){
         Random random=new Random();
         return random.nextInt(100);
     }
     public int previousNumber;
+    private FragmentBlankBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        high_or_Low=findViewById(R.id.high_low);
-        editText=findViewById(R.id.find_number);
-        checkButton=findViewById(R.id.check_button);
+        binding=FragmentBlankBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
 
         previousNumber=generateNumber();
 //        Log.i(String.valueOf(previousNumber),"da________________________________________");
 
-        checkButton.setOnClickListener(new View.OnClickListener() {
+        binding.checkButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String s=editText.getText().toString();
+                String s=binding.findNumber.getText().toString();
                 int inputedNumber = Integer.parseInt(s);
 
                 if(inputedNumber==previousNumber){
-                    high_or_Low.setText("Congrets");
+                    binding.highLow.setText("Congrets");
                     previousNumber=generateNumber();
-
                 }else if(inputedNumber>previousNumber){
-                    high_or_Low.setText("Too high");
+                    binding.highLow.setText("Too high");
                 }else{
-                    high_or_Low.setText("Too Low");
+                    binding.highLow.setText("Too Low");
                 }
             }
         });
